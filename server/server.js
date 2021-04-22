d91ca0f420c9d2bef1c54dbb4499efe53403ec03
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-04-22 09:34:04 -0400

    create schema, add function to find or create a new document by id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,13 @@
+const mongoose = require('mongoose');
+const Document = require('./Document.js');
+
+mongoose.connect('mongodb://localhost/google-docs-clone', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+
 const io = require('socket.io')(3001, {
     cors: {
         origin: 'http://localhost:3000',
@@ -5,12 +15,14 @@ const io = require('socket.io')(3001, {
     }
 })
 
+const defaultValue = "";
+
 io.on("connection", socket => {
     socket.on('get-document', documentId => {
-        const data = "";
+        const document = findOrCreateDocument(documentId);
         // creates a room where user can edit
         socket.join(documentId);
-        socket.emit('load-document', data);
+        socket.emit('load-document', document.data);
         // send changes to specific room when broadcasted
         socket.on('send-changes', delta => {
             socket.broadcast.to(documentId).emit('receive-changes', delta);
@@ -19,3 +31,12 @@ io.on("connection", socket => {
 
     console.log("Connected")
 })
+
+async function findOrCreateDocument(id) {
+    if (id == null) return;
+
+    const document = await Document.findById(id);
+
+    if (document) return document;
+    return await Document.create({ _id: id, data: defaultValue })
+}
